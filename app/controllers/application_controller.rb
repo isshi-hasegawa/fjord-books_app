33867117faa1436c7dd5896bc95dc7df6af39e81
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-01-21 21:47:41 +0900

    ユーザー登録or更新時にユーザー名、自己紹介、郵便番号、住所を保存できるよう実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username introduction postcode address])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username introduction postcode address])
+  end
 end
