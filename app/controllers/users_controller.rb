44e0874dd605ac6b518f6e0113f9cdaca680dcf8
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-01-27 05:44:26 +0900

    フォロー一覧画面・フォロワー一覧画面を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def followings
+    @title = 'Followings'
+    @users = User.find(params[:id]).followings.order(:id).page(params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'Followers'
+    @users = User.find(params[:id]).followers.order(:id).page(params[:page])
+    render 'show_follow'
+  end
 end
