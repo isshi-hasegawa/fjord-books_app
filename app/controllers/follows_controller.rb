95953f0f55f9588c231771fa8333e3dc2dafbc86
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-02-01 07:09:31 +0900

    フォロー/フォロー解除の実装を修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 class FollowsController < ApplicationController
+  before_action :set_user
+
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    current_user.follow(@user)
+    redirect_to @user
   end
 
   def destroy
-    user = Follow.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    current_user.unfollow(@user)
+    redirect_to @user
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
