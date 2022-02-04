87afb96cd325a0703672c9c0fc76cad8bef057e3
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-02-05 07:47:17 +0900

    Reportクラスのモデルテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @report = reports(:alice_report)
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test '#editable?' do
+    assert @report.editable?(@alice)
+    assert_not @report.editable?(@bob)
+  end
+
+  test '#created_on' do
+    assert_equal @report.created_on.to_s, '2022-01-01'
+  end
 end
